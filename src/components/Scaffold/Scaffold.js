fed5f715074be0e70a6623ad9770847afbc4ac1c
Fernando Andrés Avalos López <fa.avalos@uniandes.edu.co>
2022-06-14 21:02:02 -0500

    Added pagination. Render of tweets was successful

--- src/components/Scaffold/Scaffold.js
+++ src/components/Scaffold/Scaffold.js
@@ -1,9 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import Tweet from '../Tweet/Tweet';
+// import Tweet from '../Tweet/Tweet';
 
+
+// eslint-disable-next-line no-unused-vars
 const Scaffold = ({ odd }) => {
   // Aqui se hace un renderizado de la lista como tal
-  const [tweets, setTweets] = useState(null);
+
+  const [tweets, setTweets] = useState([]);
+  const [filtered, setFiltered] = useState(null);
+  const [index, setIndex] = useState(0);
+
+  const PAGE_SIZE = 10;
 
   useEffect(() => {
     fetchTweets();
@@ -11,22 +19,60 @@ const Scaffold = ({ odd }) => {
   }, []);
 
   const fetchTweets = async () => {
-    let response = await fetch('https://gist.githubusercontent.com/favalosdev/c8335d9a81f508f1d969b6a22d407a01/raw/20563f7f061f9da91f36db08a90d776e4ee0148e/reduced_tweets.json');
-    let data = await response.json();
-
-    // Has to be reduced for performance reasons
-    let mod = odd === true ? 1 : 0;
-
-    let filtered = data.filter((tweet, index) => index % 2 === mod);
-    setTweets(filtered);
+    fetch('https://gist.githubusercontent.com/favalosdev/be710f1fc60ff42ce8d3c59171f23c1b/raw/25cea926d46d7dc9e70def4f699c0437e04fb1c5/reduced_test_tweets.json')
+      .then((response) => {
+        console.log('Response', response);
+        response.json()
+          .then((data) => {
+            console.log('Data',data);
+            setFiltered(data['id']);  
+          })
+          .catch((e)=>{
+            console.log('error',e);
+          });
+      })
+      .catch((e)=>{
+        console.log('error', e);
+      });
   };
 
+  function getMoreData() {
+    if (filtered && index < filtered.length){
+      setTweets(tweets.concat(filtered.slice(index, index + PAGE_SIZE)));
+      if (index + PAGE_SIZE < filtered.length) {
+        setIndex(index + PAGE_SIZE);
+      }
+      else{
+        setIndex(filtered.length);
+      }
+    }
+    console.log('Loaded', tweets);
+  }
+
+  function show() {
+    if (filtered){
+      return(
+        <div>
+          {tweets.map((id, idx) => {
+            return(
+              <Tweet key={idx} id={id}/>
+            );
+          })}
+          <button onClick={getMoreData}>
+            Load more
+          </button>
+        </div>
+      );
+    }
+    else{
+      return(
+        <h1>LOADING</h1>
+      );
+    }
+  }
+  
   return(
-    <div>
-      {tweets.map((id) => {
-        <Tweet id={id}/>;
-      })}
-    </div>
+    <>{show()}</>
   );
 };
 
